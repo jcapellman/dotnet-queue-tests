db0af8801602ff4c61104aac7875f6c6342ef17d
Jarred Capellman <jcapellman@hotmail.com>
2018-10-03 20:37:23 -0500

    Added: Initial work on the QueryHashResponse

---
+++ RESTQueue.lib/Models/QueryHashResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace RESTQueue.lib.Models
+{
+    [DataContract]
+    public class QueryHashResponse
+    {
+        [DataMember]
+        public Guid Guid { get; set; }
+    }
+}
