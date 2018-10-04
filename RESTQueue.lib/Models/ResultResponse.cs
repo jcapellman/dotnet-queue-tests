156fea09915c59b34b1d3a3fd0fa50310453d66c
Jarred Capellman <jcapellman@hotmail.com>
2018-10-03 20:38:19 -0500

    Added: Initial work on the ResultResponse class

---
+++ RESTQueue.lib/Models/ResultResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace RESTQueue.lib.Models
+{
+    [DataContract]
+    public class ResultResponse
+    {
+        [DataMember]
+        public string Result { get; set; }
+    }
+}
