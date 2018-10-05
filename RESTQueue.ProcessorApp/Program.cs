5c78a4f6b82780d2f49a309015f8f1dd035adf52
Jarred Capellman <jcapellman@hotmail.com>
2018-10-04 21:48:49 -0500

    Added: Initial work on the ProcessorApp

---
+++ RESTQueue.ProcessorApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using RawRabbit.Context;
+using RawRabbit.vNext;
+
+namespace RESTQueue.ProcessorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = BusClientFactory.CreateDefault();
+            
+             client.SubscribeAsync<byte[]>(SubscribeMethod);
+        }
+
+        private static async Task SubscribeMethod(byte[] data, MessageContext context)
+        {
+            Console.WriteLine(context.GlobalRequestId);
+            // TODO: Run Model
+            // TODO: Save to Database
+        }
+    }
+}
