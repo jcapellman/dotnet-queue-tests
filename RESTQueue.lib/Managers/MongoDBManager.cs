d4b6a04e466050c82b4f056e66faf6645a25ae91
Jarred Capellman <jcapellman@hotmail.com>
2018-10-05 21:16:25 -0500

    Added: MongoDBManager class to handle the refactoring

---
+++ RESTQueue.lib/Managers/MongoDBManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using MongoDB.Driver;
+
+using RESTQueue.lib.Models;
+
+namespace RESTQueue.lib.Managers
+{
+    public class MongoDBManager
+    {
+        private MongoClient _client;
+        private IMongoDatabase _db;
+
+        public MongoDBManager(string host, int port)
+        {
+            var settings = new MongoClientSettings()
+            {
+                Server = new MongoServerAddress(host, port)
+            };
+
+            _client = new MongoClient(settings);
+
+            _db = _client.GetDatabase("results");
+        }
+
+        public async Task<IAsyncCursor<QueryHashResponse>> GetFromGUIDAsync(Guid guid)
+        {
+            var collection = _db.GetCollection<QueryHashResponse>("results");
+
+            return await collection.FindAsync(a => a.Guid == guid);
+        }
+    }
+}
