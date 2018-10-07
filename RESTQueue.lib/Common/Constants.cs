5d38fa1abe2e710af8d9ff132448fe1d8fbbcd7d
Jarred Capellman <jcapellman@hotmail.com>
2018-10-06 20:42:31 -0500

    Added: Constants class to the lib

---
+++ RESTQueue.lib/Common/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace RESTQueue.lib.Common
+{
+    public class Constants
+    {
+        public const string FILENAME_MODEL = "pe.model";
+
+        public const string FILENAME_SETTINGS = "appsettings.json";
+    }
+}
