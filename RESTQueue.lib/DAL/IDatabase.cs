ee0619c600259300a65e78642ad27a16aeba9800
Jarred Capellman <jcapellman@hotmail.com>
2018-10-07 13:24:21 -0500

    Added: IDatabase Interface

---
+++ RESTQueue.lib/DAL/IDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using RESTQueue.lib.Models;
+
+namespace RESTQueue.lib.DAL
+{
+    public interface IStorageDatabase
+    {
+        Task<QueryHashResponse> GetFromGUIDAsync(Guid guid);
+
+        Task Insert(QueryHashResponse item);
+    }
+}
