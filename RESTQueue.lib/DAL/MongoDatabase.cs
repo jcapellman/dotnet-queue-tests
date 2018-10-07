df82a9541a65546f648cd66cb098ebd4703170f8
Jarred Capellman <jcapellman@hotmail.com>
2018-10-07 13:24:44 -0500

    Added: Initial work on the MongoDatabase class

---
+++ RESTQueue.lib/DAL/MongoDatabase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Options;
+
+using MongoDB.Driver;
+
+using RESTQueue.lib.Common;
+using RESTQueue.lib.Models;
+
+namespace RESTQueue.lib.DAL
+{
+    public class MongoDatabase : IStorageDatabase
+    {
+        private readonly IMongoDatabase _db;
+
+        public MongoDatabase(IOptions<Settings> settings) : this(settings.Value) { }
+
+        public MongoDatabase(Settings settings)
+        {
+            var mongoSettings = new MongoClientSettings()
+            {
+                Server = new MongoServerAddress(settings.MongoHostName, settings.MongoPortNumber)
+            };
+
+            var client = new MongoClient(mongoSettings);
+
+            _db = client.GetDatabase("results");
+        }
+
+        public async Task<QueryHashResponse> GetFromGUIDAsync(Guid guid)
+        {
+            var collection = _db.GetCollection<QueryHashResponse>("results");
+
+            return (await collection.FindAsync(a => a.Guid == guid)).FirstOrDefault();
+        }
+
+        public async Task Insert(QueryHashResponse item)
+        {
+            var collection = _db.GetCollection<QueryHashResponse>("results");
+
+            await collection.InsertOneAsync(item);
+        }
+    }
+}
