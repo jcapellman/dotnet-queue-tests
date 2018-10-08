4c3e594f7e0932a57891ef0b9e3a7bdfbc3e8f75
Jarred Capellman <jcapellman@hotmail.com>
2018-10-07 20:10:19 -0500

    Added: Fallback constants for the MongoDB

--- RESTQueue.lib/Common/Constants.cs
+++ RESTQueue.lib/Common/Constants.cs
@@ -5,5 +5,8 @@
         public const string FILENAME_MODEL = "pe.model";
 
         public const string FILENAME_SETTINGS = "appsettings.json";
+
+        public const string DEFAULT_MONGO_HOSTNAME = "localhost";
+        public const string DEFAULT_MONGO_HOSTPORT = "27017";
     }
 }
