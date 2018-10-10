10cb7d086cdb0d452622cf18e96276ede893dcc2
Jarred Capellman <jcapellman@hotmail.com>
2018-10-09 20:11:16 -0500

    Added: Initial work on the IQueue Interface

---
+++ RESTQueue.lib/Queue/IQueue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RESTQueue.lib.Queue
+{
+    public interface IQueue
+    {
+        bool IsOnline();
+
+        Task<bool> AddToQueueAsync(byte[] data, Guid guid);
+    }
+}
