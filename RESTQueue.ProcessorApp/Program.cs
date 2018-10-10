ea0414a02be2db86601ead7c7b14b3e3d6bc24d1
Jarred Capellman <jcapellman@hotmail.com>
2018-10-09 20:06:04 -0500

    Change: Extensive work on the Program.cs of the ProcessorApp

--- RESTQueue.ProcessorApp/Program.cs
+++ RESTQueue.ProcessorApp/Program.cs
@@ -1,8 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 using Newtonsoft.Json;
+
+using NLog;
 
 using RawRabbit.Context;
 using RawRabbit.vNext;
@@ -12,24 +15,46 @@ using RESTQueue.lib.datascience;
 using RESTQueue.lib.DAL;
 using RESTQueue.lib.Enums;
 using RESTQueue.lib.Models;
+using RESTQueue.lib.Queue;
 
 namespace RESTQueue.ProcessorApp
 {
     public class Program
     {
         private static MongoDatabase _mongoDatabase;
+        private static RabbitQueue _queue;
         private static DSManager _dsManager;
+        private static Logger Log = LogManager.GetCurrentClassLogger();
 
         static void Main(string[] args)
         {
-            var settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(Constants.FILENAME_SETTINGS));
+            try
+            {
+                var settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(Constants.FILENAME_SETTINGS));
 
-            _mongoDatabase = new MongoDatabase(settings);
-            _dsManager = new DSManager();
+                _mongoDatabase = new MongoDatabase(settings);
+                _dsManager = new DSManager();
 
-            var client = BusClientFactory.CreateDefault();
-            
-             client.SubscribeAsync<byte[]>(SubscribeMethod);
+                if (!_mongoDatabase.IsOnline())
+                {
+                    throw new Exception("MongoDB could not be established");
+                }
+
+                _queue = new RabbitQueue(settings);
+
+                if (!_queue.IsOnline())
+                {
+                    throw new Exception("Rabbit MQ could not be established");
+                }
+
+                var client = BusClientFactory.CreateDefault();
+
+                client.SubscribeAsync<byte[]>(SubscribeMethod);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
         }
 
         private static async Task SubscribeMethod(byte[] data, MessageContext context)
