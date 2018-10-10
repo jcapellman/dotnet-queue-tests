b43f3dc788d59f042392ce0df0678493fe3d64ee
Jarred Capellman <jcapellman@hotmail.com>
2018-10-09 20:11:38 -0500

    Added: Initial work on the RabbitQueue class

---
+++ RESTQueue.lib/Queue/RabbitQueue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Options;
+
+using RawRabbit;
+using RawRabbit.Configuration;
+using RawRabbit.vNext;
+
+using RESTQueue.lib.Common;
+
+namespace RESTQueue.lib.Queue
+{
+    public class RabbitQueue : IQueue
+    {
+        private readonly IBusClient _busClient;
+
+        public RabbitQueue(IOptions<Settings> settings) : this(settings.Value) { }
+
+        public RabbitQueue(Settings settings)
+        {
+            var config = new RawRabbitConfiguration()
+            {
+                Port = settings.RabbitMQPortNumber,
+                Username = settings.RabbitMQUsername,
+                Password = settings.RabbitMQPassword,
+                Hostnames = new List<string> {settings.RabbitMQHostName}
+            };
+
+            _busClient = BusClientFactory.CreateDefault(config);
+        }
+
+        
+        public bool IsOnline() => _busClient != null;
+
+        public async Task<bool> AddToQueueAsync(byte[] data, Guid guid)
+        {
+            try
+            {
+                await _busClient.PublishAsync(data, guid);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
