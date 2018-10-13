d24697295394a79b63f30ffaff115f3a813bcb4e
Jarred Capellman <jcapellman@hotmail.com>
2018-10-13 11:52:26 -0500

    Added: Initial work on the MessageProcessor class

---
+++ RESTQueue.ProcessorApp/MessageProcessor.cs
@@ -0,0 +1,27 @@
+﻿using RESTQueue.lib.datascience;
+
+namespace RESTQueue.ProcessorApp
+{
+    public class MessageProcessor
+    {
+        private readonly DSManager _dsManager;
+
+        public bool Running { get; private set; }
+
+        public MessageProcessor(DSManager dsmanager)
+        {
+            _dsManager = dsmanager;
+        }
+
+        public bool IsMalicious(byte[] data)
+        {
+            Running = true;
+
+            var result = _dsManager.IsMalicious(data);
+
+            Running = false;
+
+            return result;
+        }
+    }
+}
