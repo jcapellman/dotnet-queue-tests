343492ad0dd9fe8a7cbaf4ac7b349129c296b470
Jarred Capellman <jcapellman@hotmail.com>
2018-10-21 09:21:18 -0400

    Added: Initial work on the StorageManager class

---
+++ RESTQueue.lib/Managers/StorageManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using NLog;
+
+using RESTQueue.lib.DAL;
+using RESTQueue.lib.Models;
+
+namespace RESTQueue.lib.Managers
+{
+    public class StorageManager
+    {
+        private static Logger Log = LogManager.GetCurrentClassLogger();
+
+        private List<IStorageDatabase> _storageDatabases;
+
+        public bool InitializeStorage(params IStorageDatabase[] databases)
+        {
+            _storageDatabases = new List<IStorageDatabase>(databases.Length);
+
+            foreach (var database in databases)
+            {
+                _storageDatabases.Add(database);
+            }
+
+            return true;
+        }
+
+        public async Task<bool> InsertAsync(QueryHashResponse response)
+        {
+            foreach (var database in _storageDatabases)
+            {
+                var result = await database.Insert(response);
+
+                if (result)
+                {
+                    Log.Debug($"{database.Name} inserted {response} successfully");
+
+                    return true;
+                }
+            }
+
+            Log.Warn($"Could not log {response} in any storage device");
+
+            return false;
+        }
+    }
+}
