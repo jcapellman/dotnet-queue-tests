6191df673d5018b812c27d30fb0e9385ef62094f
Jarred Capellman <jcapellman@hotmail.com>
2018-10-21 08:54:56 -0400

    Added: Initial work on the LiteDBDatabase class

---
+++ RESTQueue.lib/DAL/LiteDBDatabase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using RESTQueue.lib.Models;
+
+namespace RESTQueue.lib.DAL
+{
+    public class LiteDBDatabase : IStorageDatabase
+    {
+        private const string DB_FILE_NAME = "litedb.db";
+
+        public Task<QueryHashResponse> GetFromGUIDAsync(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Insert(QueryHashResponse item)
+        {
+            await Task.Run(() =>
+            {
+                using (var db = new LiteDB.LiteDatabase(DB_FILE_NAME))
+                {
+                    var collection = db.GetCollection<QueryHashResponse>();
+
+                    collection.Insert(item);
+                }
+            });
+        }
+
+        public bool IsOnline()
+        {
+            return true;
+        }
+
+        public string Name => "LiteDB";
+    }
+}
