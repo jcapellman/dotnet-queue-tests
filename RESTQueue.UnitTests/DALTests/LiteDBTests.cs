fd64736a6f875a08f2cef896f34e7d3cb0d096aa
Jarred Capellman <jcapellman@hotmail.com>
2018-10-27 18:32:03 -0500

    Added: Initial work on the LiteDBTests class

---
+++ RESTQueue.UnitTests/DALTests/LiteDBTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RESTQueue.lib.DAL;
+using RESTQueue.lib.Enums;
+using RESTQueue.lib.Models;
+
+namespace RESTQueue.UnitTests.DALTests
+{
+    [TestClass]
+    public class LiteDBTests
+    {
+        [TestMethod]
+        public async Task InsertNullTest()
+        {
+            var liteDB = new LiteDBDatabase();
+
+            var result = await liteDB.Insert(null);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public async Task InsertDefaultTest()
+        {
+            var liteDB = new LiteDBDatabase();
+            
+            var result = await liteDB.Insert(new QueryHashResponse());
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public async Task InsertGoodTest()
+        {
+            var liteDB = new LiteDBDatabase();
+
+            var queryHashResponse = new QueryHashResponse
+            {
+                Guid = Guid.NewGuid(),
+                Status = ResponseStatus.PENDING,
+                MD5Hash = "1244",
+                ErrorMessage = string.Empty,
+                IsMalicious = false
+            };
+
+            var result = await liteDB.Insert(queryHashResponse);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
