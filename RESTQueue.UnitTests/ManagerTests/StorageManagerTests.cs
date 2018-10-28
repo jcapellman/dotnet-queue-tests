a25aec8d5549673f21466050164ba2386f19bb72
Jarred Capellman <jcapellman@hotmail.com>
2018-10-27 22:51:36 -0500

    Added: Initial work on the StorageManagerTests class

---
+++ RESTQueue.UnitTests/ManagerTests/StorageManagerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RESTQueue.lib.DAL;
+using RESTQueue.lib.Managers;
+
+namespace RESTQueue.UnitTests.ManagerTests
+{
+    [TestClass]
+    public class StorageManagerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullConstructor()
+        {
+            var storageManger = new StorageManager(null);
+        }
+
+        [TestMethod]
+        public void InitializeConstructor()
+        {
+            var storageManger = new StorageManager(new List<IStorageDatabase>
+            {
+                new LiteDBDatabase()
+            });
+        }
+
+        [TestMethod]
+        public void GetStorageDatabaseStatusesNull()
+        {
+            var storageManger = new StorageManager(new List<IStorageDatabase>
+            {
+                new LiteDBDatabase()
+            });
+
+            var result = storageManger.GetStorageDatabaseStatuses();
+
+            Assert.IsNotNull(result);
+
+            Assert.IsTrue(result.Any());
+        }
+    }
+}
