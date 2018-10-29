c2baf785bed8d9eefce9eee41dee09ce354187ee
Jarred Capellman <jcapellman@hotmail.com>
2018-10-28 21:21:25 -0500

    Added: Initial commit of the RabbitQueueTests class

---
+++ RESTQueue.UnitTests/QueueTests/RabbitQueueTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RESTQueue.lib.Common;
+using RESTQueue.lib.Queue;
+
+namespace RESTQueue.UnitTests.QueueTests
+{
+    [TestClass]
+    public class RabbitQueueTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void InitializeNull()
+        {
+            var rabbit = new RabbitQueue(settings: (Settings) null);
+        }
+    }
+}
