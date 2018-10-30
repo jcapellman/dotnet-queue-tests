79a16cf9940d75ac8a94e5de4f7f25c2ef8bc630
Jarred Capellman <jcapellman@hotmail.com>
2018-10-29 21:05:17 -0500

    Added: Additional Unit Tests to the RedisCacheTests class

--- RESTQueue.UnitTests/CacheTests/RedisCacheTest.cs
+++ RESTQueue.UnitTests/CacheTests/RedisCacheTest.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Threading.Tasks;
 
+using Microsoft.Extensions.Options;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using RESTQueue.lib.CacheDAL;
 using RESTQueue.lib.Common;
+using RESTQueue.lib.Models;
 
 namespace RESTQueue.UnitTests.CacheTests
 {
@@ -15,6 +18,43 @@ namespace RESTQueue.UnitTests.CacheTests
         public void InitializeNull()
         {
             var redis = new RedisCache((Settings) null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void InitializeIOptionsNull()
+        {
+            var redis = new RedisCache((IOptions<Settings>)null);
+        }
+        
+        private Settings RedisSettings => new Settings
+        {
+            CacheHostName = "localhost",
+            CacheEnabled = true
+        };
+        
+        [TestMethod]
+        public async Task AddResponseAsyncNull()
+        {
+            var redis = new RedisCache(RedisSettings);
+
+            await redis.AddResponseAsync(null);
+        }
+        
+        [TestMethod]
+        public async Task AddResponseAsyncEmpty()
+        {
+            var redis = new RedisCache(RedisSettings);
+
+            await redis.AddResponseAsync(new QueryHashResponse());
+        }
+
+        [TestMethod]
+        public async Task GetResponseNull()
+        {
+            var redis = new RedisCache(RedisSettings);
+
+            await redis.GetResponseAsync(null);
         }
     }
 }
