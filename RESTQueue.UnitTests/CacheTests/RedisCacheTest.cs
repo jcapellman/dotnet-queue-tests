efa87da9f5c19c59879f3fbf373a97eafe12c427
Jarred Capellman <jcapellman@hotmail.com>
2018-10-29 20:22:35 -0500

    Added: Initial commit of the RedisCacheTest class

---
+++ RESTQueue.UnitTests/CacheTests/RedisCacheTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RESTQueue.lib.CacheDAL;
+using RESTQueue.lib.Common;
+
+namespace RESTQueue.UnitTests.CacheTests
+{
+    [TestClass]
+    public class RedisCacheTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void InitializeNull()
+        {
+            var redis = new RedisCache((Settings) null);
+        }
+    }
+}
