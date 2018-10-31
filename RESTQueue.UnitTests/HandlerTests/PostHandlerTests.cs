44c92b633f073393db725e813e0d0074a42b5814
Jarred Capellman <jcapellman@hotmail.com>
2018-10-30 20:19:26 -0500

    Added: Initial work on the PostHandlerTests class

---
+++ RESTQueue.UnitTests/HandlerTests/PostHandlerTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RESTQueue.lib.Handlers;
+
+namespace RESTQueue.UnitTests.HandlerTests
+{
+    [TestClass]
+    public class PostHandlerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task InitializeNull()
+        {
+            var handler = new PostHandler(null);
+
+            var result = await handler.SubmitQueryHashAsync(null);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task InitializeNullFile()
+        {
+            var handler = new PostHandler("localhosty");
+
+            var result = await handler.SubmitQueryHashAsync(null);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public async Task InitializeBadHost()
+        {
+            var handler = new PostHandler("localhosty");
+
+            var file = Path.GetTempFileName();
+
+            var result = await handler.SubmitQueryHashAsync(file);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public async Task InitializeNotFoundFile()
+        {
+            var handler = new PostHandler("localhosty");
+
+            var file = @"c:\tempo\tempy";
+
+            var result = await handler.SubmitQueryHashAsync(file);
+
+            Assert.IsNull(result);
+        }
+    }
+}
