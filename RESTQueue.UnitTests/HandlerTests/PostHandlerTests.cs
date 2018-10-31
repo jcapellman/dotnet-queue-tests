c688decd673143aa383ca5d45692b049d9777ce1
Jarred Capellman <jcapellman@hotmail.com>
2018-10-30 20:45:19 -0500

    Added: Mocking Responses for the PostHandlerTests class

--- RESTQueue.UnitTests/HandlerTests/PostHandlerTests.cs
+++ RESTQueue.UnitTests/HandlerTests/PostHandlerTests.cs
@@ -4,7 +4,15 @@ using System.Threading.Tasks;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using Newtonsoft.Json;
+
+using RESTQueue.lib.Enums;
 using RESTQueue.lib.Handlers;
+
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
 
 namespace RESTQueue.UnitTests.HandlerTests
 {
@@ -57,6 +65,81 @@ namespace RESTQueue.UnitTests.HandlerTests
             var result = await handler.SubmitQueryHashAsync(file);
 
             Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(JsonSerializationException))]
+        public async Task InitializeAndSubmitBadResponse()
+        {
+            FluentMockServer server = null;
+
+            try
+            {
+                server = FluentMockServer.Start(new FluentMockServerSettings {Urls = new[] {"http://+:5010"}});
+
+                server
+                    .Given(Request.Create().WithPath("/Query").UsingGet())
+                    .RespondWith(
+                        Response.Create()
+                            .WithStatusCode(200)
+                            .WithBody(@"{ ""msg"": ""Hello world!"" }")
+                    );
+
+                var handler = new PostHandler("http://localhost:5010/");
+
+                var file = Path.GetTempFileName();
+
+                var result = await handler.SubmitQueryHashAsync(file);
+
+                Assert.IsNull(result);
+
+            }
+            finally
+            {
+                server?.Stop();
+            }
+        }
+
+        [TestMethod]
+        public async Task InitializeAndSubmitGoodResponse()
+        {
+            FluentMockServer server = null;
+
+            try
+            {
+                server = FluentMockServer.Start(new FluentMockServerSettings { Urls = new[] { "http://+:5010" } });
+
+                server
+                    .Given(Request.Create().WithPath("/Query").UsingPost())
+                    .RespondWith(
+                        Response.Create()
+                            .WithStatusCode(200)
+                            .WithBody(@"{
+                              ""guid"": ""00000000-0000-0000-0000-000000000000"",
+                              ""status"": ""SUBMITTED"",
+                              ""mD5Hash"": ""12345"",
+                              ""errorMessage"": null,
+                              ""isMalicious"": false
+                            }")
+                    );
+
+                var handler = new PostHandler("http://localhost:5010/");
+
+                var file = Path.GetTempFileName();
+
+                var result = await handler.SubmitQueryHashAsync(file);
+
+                Assert.IsNotNull(result);
+                Assert.AreEqual(ResponseStatus.SUBMITTED, result.Status);
+                Assert.AreEqual("12345", result.MD5Hash);
+                Assert.AreEqual(false, result.IsMalicious);
+                Assert.AreEqual(Guid.Empty, result.Guid);
+                Assert.IsNull(result.ErrorMessage);
+            }
+            finally
+            {
+                server?.Stop();
+            }
         }
     }
 }
