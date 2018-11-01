f06d69195c41f30f6ea6d04edc38d89d0660c4f8
Jarred Capellman <jcapellman@hotmail.com>
2018-10-31 20:23:19 -0500

    Added: Initial MessageProcessorTests Unit Tests

---
+++ RESTQueue.UnitTests/ProcessorAppTests/MessageProcessorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RESTQueue.lib.Common;
+using RESTQueue.lib.datascience;
+using RESTQueue.ProcessorApp;
+
+namespace RESTQueue.UnitTests.ProcessorAppTests
+{
+    [TestClass]
+    public class MessageProcessorTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void InitializeNull()
+        {
+            var messageProcessor = new MessageProcessor(null);
+        }
+
+        private static DSManager ValidDSManager => new DSManager();
+
+        [TestInitialize]
+        public void Setup()
+        {
+            File.WriteAllText(Constants.FILENAME_MODEL, string.Empty);
+        }
+
+        [TestMethod]
+        public void InitializeProperNullMalicious()
+        {
+            var messageProcessor = new MessageProcessor(ValidDSManager);
+
+            messageProcessor.IsMalicious(null);
+        }
+    }
+}
