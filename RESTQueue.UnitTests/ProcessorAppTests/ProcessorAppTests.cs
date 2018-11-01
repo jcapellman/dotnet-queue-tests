ecaffd0fc3c728c1f140bc5ccb90dc36ef740aa4
Jarred Capellman <jcapellman@hotmail.com>
2018-10-31 20:49:35 -0500

    AddeD: Initial work on the ProcessorAppTests class

---
+++ RESTQueue.UnitTests/ProcessorAppTests/ProcessorAppTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RawRabbit.Context;
+
+using RESTQueue.ProcessorApp;
+
+namespace RESTQueue.UnitTests.ProcessorAppTests
+{
+    [TestClass]
+    public class ProcessorAppTests
+    {
+        [TestMethod]
+        public void InitTest()
+        {
+            Program.Initialize();
+        }
+
+        [TestMethod]
+        public async Task SubscribeNullNullTest()
+        {
+            await Program.SubscribeMethod(null, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task SubscribeNullDataTest()
+        {
+            await Program.SubscribeMethod(File.ReadAllBytes(Path.GetTempFileName()), null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task SubscribeContextDataTest()
+        {
+            await Program.SubscribeMethod(File.ReadAllBytes(Path.GetTempFileName()), new AdvancedMessageContext());
+        }
+    }
+}
